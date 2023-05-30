75d4eca5fd5a9bdfd766076deab034ff90453f87
Tom Richardson <firecracker37@gmail.com>
2023-05-29 21:11:38 -0500

    End of Night

--- frontend/src/screens/ProfileScreen.jsx
+++ frontend/src/screens/ProfileScreen.jsx
@@ -1,7 +1,79 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import FormContainer from '../components/FormContainer';
+import { toast } from 'react-toastify';
 
 export default function ProfileScreen() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const passwordsMatch = password === confirmPassword;
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    console.log('submit');
+  }
   return (
-    <div>ProfileScreen</div>
+    <FormContainer>
+      <form className="space-y-4" onSubmit={submitHandler}>
+      <div>
+        <label htmlFor="name" className="text-gray-800">Name</label>
+        <input
+          type="text"
+          id="name"
+          className="block w-full border border-gray-400 rounded px-3 py-2 mt-1 focus:outline-none focus:border-gray-500"
+          placeholder="Enter your name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="email" className="text-gray-800">Email</label>
+        <input
+          type="email"
+          id="email"
+          className="block w-full border border-gray-400 rounded px-3 py-2 mt-1 focus:outline-none focus:border-gray-500"
+          placeholder="Enter your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password" className="text-gray-800">Password</label>
+        <input
+          type="password"
+          id="password"
+          className="block w-full border border-gray-400 rounded px-3 py-2 mt-1 focus:outline-none focus:border-gray-500"
+          placeholder="Enter your password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="confirmPassword" className="text-gray-800">Confirm Password</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          className={`block w-full border rounded px-3 py-2 mt-1 focus:outline-none ${
+            passwordsMatch ? 'border-gray-400' : 'border-red-500'
+          }`}
+          placeholder="Confirm your password"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+        {!passwordsMatch && (
+          <p className="text-red-500 text-sm mt-1">Passwords do not match</p>
+        )}
+      </div>
+      <button
+        type="submit"
+        className="bg-gray-800 text-white rounded px-4 py-2 hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
+      >
+        Register
+      </button>
+    </form>
+    </FormContainer>
   )
 }
